c321247770bcc8ef21dbf023e6a6f8f9fd49dfb9
Trung Phạm <trungnhot12@gmail.com>
2022-10-01 11:42:35 +0700

    up

--- js/custom.js
+++ js/custom.js
@@ -46,3 +46,41 @@ function md5Generator() {
   document.querySelector("#result").innerHTML =
     "<h3>Result</h3><p id='content-result'>" + result + "</p>";
 }
+
+
+/**
+ * Accepts either a URL or querystring and returns an object associating 
+ * each querystring parameter to its value. 
+ *
+ * Returns an empty object if no querystring parameters found.
+ */
+ function getUrlParams(urlOrQueryString) {
+  if ((i = urlOrQueryString.indexOf('?')) >= 0) {
+    const queryString = urlOrQueryString.substring(i+1);
+    if (queryString) {
+      return _mapUrlParams(queryString);
+    } 
+  }
+  
+  return {};
+}
+
+/**
+ * Helper function for `getUrlParams()`
+ * Builds the querystring parameter to value object map.
+ *
+ * @param queryString {string} - The full querystring, without the leading '?'.
+ */
+function _mapUrlParams(queryString) {
+  return queryString    
+    .split('&') 
+    .map(function(keyValueString) { return keyValueString.split('=') })
+    .reduce(function(urlParams, [key, value]) {
+      if (Number.isInteger(parseInt(value)) && parseInt(value) == value) {
+        urlParams[key] = parseInt(value);
+      } else {
+        urlParams[key] = decodeURI(value);
+      }
+      return urlParams;
+    }, {});
+}
