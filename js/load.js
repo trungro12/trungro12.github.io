0635531d89739678c6063859c3b8a9e89198f8e9
Trung Phạm <trungnhot12@gmail.com>
2022-10-01 18:42:43 +0700

    up

--- js/load.js
+++ js/load.js
@@ -1,22 +1,22 @@
 
-var currenturl = location.href.split('?')[0];
-$("#allvoucher").click(function () {
-    location.href = currenturl;
-});
-$("#shopee").click(function () {
-    location.href = currenturl + "?type=shopee";
-});
-$("#tiki").click(function () {
-    location.href = currenturl + "?type=tiki";
-});
-$("#game").click(function () {
-    location.href = currenturl + "?type=game";
-});
+// var currenturl = location.href.split('?')[0];
+// $("#allvoucher").click(function () {
+//     location.href = currenturl;
+// });
+// $("#shopee").click(function () {
+//     location.href = currenturl + "?type=shopee";
+// });
+// $("#tiki").click(function () {
+//     location.href = currenturl + "?type=tiki";
+// });
+// $("#game").click(function () {
+//     location.href = currenturl + "?type=game";
+// });
 
-let urlParams = getUrlParams(location.search); // Assume location.search = "?a=1&b=2b2"
-console.log(urlParams);
-if (urlParams['type']) $("#" + urlParams['type'] + "").addClass("btn-active");
-else $("#allvoucher").addClass("btn-active");
+// let urlParams = getUrlParams(location.search); // Assume location.search = "?a=1&b=2b2"
+// console.log(urlParams);
+// if (urlParams['type']) $("#" + urlParams['type'] + "").addClass("btn-active");
+// else $("#allvoucher").addClass("btn-active");
 
 var loadContents = $("#loadContents");
 if (urlParams['type'] == 'shopee') loadContent(loadContents, "shopee.html");
