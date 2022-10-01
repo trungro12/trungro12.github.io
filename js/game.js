7197ef12cd6f4b0ab4802e5f8a0b2cac92a5468f
Trung Phạm <trungnhot12@gmail.com>
2022-10-01 19:11:31 +0700

    up

--- js/game.js
+++ js/game.js
@@ -4,7 +4,7 @@ function gameloader() {
     var image = ['https://cdn.akamai.steamstatic.com/steam/apps/349040/header.jpg'];
     var contentHTML = '<div class="row">';
     game.forEach(function (content, index) {
-        contentHTML += '<a target="_blank" href="' + url[index] + '"><div class="game-content"><img src="' + image[index] + '"><h3>' + content + '</h3></div></a>';
+        contentHTML += '<a target="_blank" rel="noopener" href="' + url[index] + '"><div class="game-content"><img src="' + image[index] + '"><h3>' + content + '</h3></div></a>';
     });
     contentHTML += '</div>';
     $("#gameloader").html(contentHTML);
