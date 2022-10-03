e003d40b0492dd5bdfb9dbc166c6b8c8ec5bbc9a
Trung Pham <trungnhot12@gmail.com>
2022-10-03 18:20:24 +0700

    up

--- js/game.js
+++ js/game.js
@@ -1,12 +1,33 @@
 function gameloader() {
-    var game = ['NARUTO SHIPPUDEN: Ultimate Ninja STORM 4'];
-    var url = ['https://store.steampowered.com/app/349040/NARUTO_SHIPPUDEN_Ultimate_Ninja_STORM_4/'];
-    var image = ['https://cdn.akamai.steamstatic.com/steam/apps/349040/header.jpg'];
-    var contentHTML = '<div class="row">';
-    game.forEach(function (content, index) {
-        contentHTML += '<a target="_blank" rel="noopener" href="' + url[index] + '"><div class="game-content"><img src="' + image[index] + '"><h3>' + content + '</h3></div></a>';
-    });
-    contentHTML += '</div>';
-    $("#gameloader").html(contentHTML);
+  var game = ["NARUTO SHIPPUDEN: Ultimate Ninja STORM 4"];
+  var url = [
+    "https://store.steampowered.com/app/349040/NARUTO_SHIPPUDEN_Ultimate_Ninja_STORM_4/",
+  ];
+  var image = [
+    "https://cdn.akamai.steamstatic.com/steam/apps/349040/header.jpg",
+  ];
+  var contentHTML = '<div class="coupon-section">';
+  contentHTML += '<div class="row">';
+  contentHTML += "<h3>Game có trong video trên Tiktok của mình</h3>";
+  game.forEach(function (content, index) {
+    contentHTML += '<div class="col-lg-3">';
+    contentHTML +=
+      '<a target="_blank" rel="noopener" href="' + url[index] + '">';
+
+    contentHTML +=
+      '<div class="coupon-content"><img width="450" height="200" alt="' +
+      content +
+      '" src="' +
+      image[index] +
+      '"><p class="coupon-text">' +
+      content +
+      "</p></div>";
+
+    contentHTML += "</a>";
+    contentHTML += "</div>";
+  });
+  contentHTML += "</div>";
+  contentHTML += "</div>";
+  $("#gameloader").html(contentHTML);
 }
 gameloader();
