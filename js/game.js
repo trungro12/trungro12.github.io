72948f54ad64260c1a66ad34e65082b11d0e4c33
Trung Pham <trungnhot12@gmail.com>
2022-10-03 18:27:17 +0700

    up

--- js/game.js
+++ js/game.js
@@ -1,5 +1,5 @@
 function gameloader() {
-  coupon();
+  coupon("#gameloader");
   var game = ["NARUTO SHIPPUDEN: Ultimate Ninja STORM 4"];
   var url = [
     "https://store.steampowered.com/app/349040/NARUTO_SHIPPUDEN_Ultimate_Ninja_STORM_4/",
