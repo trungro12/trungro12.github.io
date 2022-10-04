f5acdfce0b00f8a8bfd6847d6f0831071835d56a
Trung Phạm <trungnhot12@gmail.com>
2022-10-04 13:46:47 +0700

    up

--- js/custom.js
+++ js/custom.js
@@ -82,9 +82,9 @@ function runScripts() {
 
 // spinner loading 
 function spinnerLoading(element, show = true, size = 2) {
-  var id = "spinner-" + element.replace("#", "").replace(".", "") + Math.random().toString(36).slice(2, 7);
+  var id = "spinner-" + element.replace("#", "").replace(".", "").trim();
   var spinner = $("#" + id);
-  if (spinner.length == 0) $('<div id="' + id + '" class="spinner-border text-primary" style="scale: ' + size + ';"></div>').insertBefore(element);
+  if (spinner.length == 0) $('<div id="' + id + '" class="spinner-border text-primary" style="scale: ' + size + ';"></div>').insertAfter(element);
   if (show) spinner.show();
   else spinner.hide();
 }
