8ec463ba776beccab6b1305608cd44effae1cd22
Trung Pham <trungnhot12@gmail.com>
2022-10-04 16:59:38 +0700

    up

---
+++ js/coupon/show-coupon.js
@@ -0,0 +1,2 @@
+// show coupon at id coupon-show
+coupon("#coupon-show", null, 8);
--- js/game.js
+++ js/game.js
@@ -7,37 +7,45 @@ function gameloader() {
   var image = [
     "https://cdn.akamai.steamstatic.com/steam/apps/349040/header.jpg",
   ];
-  var video = ['https://cdn.akamai.steamstatic.com/steam/apps/256660477/movie_max.webm?t=1454603660'];
+  var video = [
+    "https://cdn.akamai.steamstatic.com/steam/apps/256660477/movie_max.webm?t=1454603660",
+  ];
 
   var contentHTML = '<div class="coupon-section">';
   contentHTML += '<div class="row">';
   contentHTML += "<h3>Game có trong video trên Tiktok của mình</h3>";
-  var style = '';
+  var style = "";
   var gameNumber = game.length;
   if (gameNumber == 1) style = 'style="margin:auto;"';
 
   game.forEach(function (content, index) {
-    var imageContent = '<img width="450" height="200" alt="' +
+    var imageContent =
+      '<img width="450" height="200" alt="' +
       content +
       '" src="' +
       image[index] +
       '">';
-    contentHTML += '<div ' + style + ' class="col-lg-6">';
+    contentHTML += "<div " + style + ' class="col-lg-6">';
     contentHTML +=
       '<a target="_blank" rel="noopener" href="' + url[index] + '">';
-    contentHTML +=
-      '<div style="height: 310px;" class="coupon-content">';
+    contentHTML += '<div style="height: 310px;" class="coupon-content">';
     if (video[index]) {
       var autoplay = "";
-      if (gameNumber == 1) autoplay = 'autoplay';
-      contentHTML += '<video poster="' + image[index] + '" style="width: auto;max-height: 220px;" loop controls="" name="media" __idm_id__="720897" ' + autoplay + '><source src="' + video[index] + '" type="video/webm">' + imageContent + '</video>';
-    }
-    else {
+      if (gameNumber == 1) autoplay = "autoplay";
+      contentHTML +=
+        '<video poster="' +
+        image[index] +
+        '" style="width: auto;max-height: 220px;" loop controls="" name="media" __idm_id__="720897" ' +
+        autoplay +
+        '><source src="' +
+        video[index] +
+        '" type="video/webm">' +
+        imageContent +
+        "</video>";
+    } else {
       contentHTML += imageContent;
     }
-    contentHTML += '<p class="coupon-text">' +
-      content +
-      "</p></div>";
+    contentHTML += '<p class="coupon-text">' + content + "</p></div>";
 
     contentHTML += "</a>";
     contentHTML += "</div>";
