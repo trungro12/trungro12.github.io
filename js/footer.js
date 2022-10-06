e2816a2bf05b6fa28b3b1ff6b042c0aed58e7be1
Trung Pham <trungnhot12@gmail.com>
2022-10-06 17:11:37 +0700

    up

--- js/footer.js
+++ js/footer.js
@@ -1,12 +1,39 @@
 const buttonLoading = [
-    ".atEQPOIVFSDFSDG-btn-keyword a",
-    ".atEQPOIVFSDFSDG-nav-link",
-    ".atEQPOIVFSDFSDG-page-link",
-    ".atEQPOIVFSDFSDG-btn-search"
-  ];
+  ".atEQPOIVFSDFSDG-btn-keyword a",
+  ".atEQPOIVFSDFSDG-nav-link",
+  ".atEQPOIVFSDFSDG-page-link",
+  ".atEQPOIVFSDFSDG-btn-search",
+];
 
-  buttonLoading.forEach(function (element) {
-    $(element).click(function(){
-        console.log('Đang Tải Dữ Liệu. Bạn chờ một chút nha !');
-    });
-  });
+buttonLoading.forEach(function (element) {
+  waitForElementToDisplay(
+    element,
+    function () {
+      $(element).click(function () {
+        console.log("Đang Tải Dữ Liệu. Bạn chờ một chút nha !");
+      });
+    },
+    1000,
+    9000
+  );
+});
+
+function waitForElementToDisplay(
+  selector,
+  callback,
+  checkFrequencyInMs,
+  timeoutInMs
+) {
+  var startTimeInMs = Date.now();
+  (function loopSearch() {
+    if (document.querySelector(selector) != null) {
+      callback();
+      return;
+    } else {
+      setTimeout(function () {
+        if (timeoutInMs && Date.now() - startTimeInMs > timeoutInMs) return;
+        loopSearch();
+      }, checkFrequencyInMs);
+    }
+  })();
+}
