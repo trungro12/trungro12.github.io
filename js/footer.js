5871b252d1d7f2e34debe050b3514eec341468a7
Trung Phạm <trungnhot12@gmail.com>
2022-10-06 13:16:57 +0700

    up

--- js/footer.js
+++ js/footer.js
@@ -1,3 +1,12 @@
+$.getScript("/plugins/toast/dist/js/nice-toast-js.min.js", function(data, textStatus, jqxhr) {
+  console.log(data); // data returned
+  console.log(textStatus); // success
+  console.log(jqxhr.status); // 200
+  console.log('Load was performed.');
+  console.log(typeof $.niceToast);
+  console.log(typeof niceToast);
+});
+
 const buttonLoading = [
   ".atEQPOIVFSDFSDG-nav-link",
   ".atEQPOIVFSDFSDG-page-link",
@@ -22,19 +31,10 @@ waitForElementToDisplay(
 );
 
 function buttonLoadingToast() {
-  // console.log("OK");
+  console.log("OK");
   // if (typeof $.niceToast === 'undefined' || typeof niceToast === 'undefined') {
-
-    $.getScript("/plugins/toast/dist/js/nice-toast-js.min.js", function(data, textStatus, jqxhr) {
-      console.log(data); // data returned
-      console.log(textStatus); // success
-      console.log(jqxhr.status); // 200
-      console.log('Load was performed.');
-      console.log(typeof $.niceToast);
-      console.log(typeof niceToast);
-    });
   // }
-  // $.niceToast.info('Đang Tải Dữ Liệu. Bạn chờ một chút nha !');
+  $.niceToast.info('Đang Tải Dữ Liệu. Bạn chờ một chút nha !');
 }
 
 function waitForElementToDisplay(
