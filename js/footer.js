3f6c2a780ab041c07ba559523c3230f54013b8c8
Trung Pham <trungnhot12@gmail.com>
2022-10-06 23:52:40 +0700

    up

---
+++ js/footer.js
@@ -0,0 +1,37 @@
+if (getUrlParams(location.search)["type"] == "game") {
+  $("#coupon-show-default").html("");
+}
+
+$(function () {
+  $("#allvoucher").click(function () {
+    location.href = currenturl;
+  });
+  $("#shopee").click(function () {
+    location.href = currenturl + "?type=shopee";
+  });
+  $("#tiki").click(function () {
+    location.href = currenturl + "?type=tiki";
+  });
+  $("#game").click(function () {
+    location.href = currenturl + "?type=game";
+  });
+});
+
+var currenturl = location.href.replace("#", "");
+currenturl = currenturl.split("?")[0];
+let urlParams = getUrlParams(location.search);
+
+var loadscript = document.createElement("script");
+loadscript.type = "text/javascript";
+
+if (urlParams["type"]) {
+  $("#" + urlParams["type"] + "").addClass("btn-active");
+  // loadscript.src = "js/load.js";
+} else {
+  $("#allvoucher").addClass("btn-active");
+  // loadscript.setAttribute("lazy", "");
+  // loadscript.setAttribute("data-src", "js/load.js");
+}
+loadscript.setAttribute("lazy", "");
+loadscript.setAttribute("data-src", "js/load.js");
+$("#loadScripts").append(loadscript);
