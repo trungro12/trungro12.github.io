c76eb388839f5d46c08e4eea5b9e11a55a1337d7
Trung Phạm <trungnhot12@gmail.com>
2022-10-06 13:27:27 +0700

    up

--- js/footer.js
+++ js/footer.js
@@ -1,41 +1,39 @@
-$.getScript("/plugins/toast/dist/js/nice-toast-js.min.js", function(data, textStatus, jqxhr) {
-  console.log(data); // data returned
-  console.log(textStatus); // success
-  console.log(jqxhr.status); // 200
-  console.log('Load was performed.');
-  console.log(typeof $.niceToast);
-  console.log(typeof niceToast);
-});
-
 const buttonLoading = [
   ".atEQPOIVFSDFSDG-nav-link",
   ".atEQPOIVFSDFSDG-page-link",
   ".atEQPOIVFSDFSDG-btn-search",
   ".atEQPOIVFSDFSDG-btn-keyword",
 ];
-waitForElementToDisplay(
-  ".atEQPOIVFSDFSDG-modal",
-  function () {
-    buttonLoading.forEach(function (el) {
-      var buttons = document.querySelectorAll(el);
-      for (var button of buttons) {
-        button.addEventListener('click', function (event) {
-          buttonLoadingToast();
-        })
-      }
 
-    });
-  },
-  1000,
-  9000
-);
+$.getScript("/plugins/toast/dist/js/nice-toast-js.min.js", function (data, textStatus, jqxhr) {
+  // console.log(data); // data returned
+  // console.log(textStatus); // success
+  // console.log(jqxhr.status); // 200
+  // console.log('Load was performed.');
+  // console.log(typeof $.niceToast); // function
+  waitForElementToDisplay(
+    ".atEQPOIVFSDFSDG-modal",
+    function () {
+      buttonLoading.forEach(function (el) {
+        var buttons = document.querySelectorAll(el);
+        for (var button of buttons) {
+          button.addEventListener('click', function (event) {
+            buttonLoadingToast();
+          })
+        }
 
-function buttonLoadingToast() {
-  console.log("OK");
-  // if (typeof $.niceToast === 'undefined' || typeof niceToast === 'undefined') {
-  // }
-  $.niceToast.info('Đang Tải Dữ Liệu. Bạn chờ một chút nha !');
-}
+      });
+    },
+    1000,
+    9000
+  );
+
+  function buttonLoadingToast() {
+    console.log("OK");
+    $.niceToast.info('Đang Tải Dữ Liệu. Bạn chờ một chút nha !');
+  }
+
+});
 
 function waitForElementToDisplay(
   selector,
