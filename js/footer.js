a4ad09c398bb66db47191e5df7528addc26a19ec
Trung Pham <trungnhot12@gmail.com>
2022-10-06 17:07:46 +0700

    up

--- js/footer.js
+++ js/footer.js
@@ -1,12 +1,12 @@
 const buttonLoading = [
-    "atEQPOIVFSDFSDG-btn-keyword a",
-    "atEQPOIVFSDFSDG-nav-link",
-    "atEQPOIVFSDFSDG-page-link",
-    "atEQPOIVFSDFSDG-btn-search"
+    ".atEQPOIVFSDFSDG-btn-keyword a",
+    ".atEQPOIVFSDFSDG-nav-link",
+    ".atEQPOIVFSDFSDG-page-link",
+    ".atEQPOIVFSDFSDG-btn-search"
   ];
 
-  buttonLoading.forEach(function (event) {
-    $(this).click(function(){
+  buttonLoading.forEach(function (element) {
+    $(element).click(function(){
         console.log('Đang Tải Dữ Liệu. Bạn chờ một chút nha !');
     });
   });
