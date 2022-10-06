54cc5f964ae0b2b9e7595c4789ad2552a76376bd
Trung Pham <trungnhot12@gmail.com>
2022-10-06 17:04:42 +0700

    up

--- js/footer.js
+++ js/footer.js
@@ -1,16 +1,12 @@
 const buttonLoading = [
-  "atEQPOIVFSDFSDG-btn-keyword a",
-  "atEQPOIVFSDFSDG-nav-link",
-  "atEQPOIVFSDFSDG-page-link",
-  "atEQPOIVFSDFSDG-btn-search",
-];
+    "atEQPOIVFSDFSDG-btn-keyword a",
+    "atEQPOIVFSDFSDG-nav-link",
+    "atEQPOIVFSDFSDG-page-link",
+    "atEQPOIVFSDFSDG-btn-search"
+  ];
 
-buttonLoading.forEach(function (event) {
-  $(this).ready(function () {
-    $(this).click(function () {
-      var toastLoad = $.niceToast.info(
-        "Đang Tải Dữ Liệu. Bạn chờ một chút nha !"
-      );
+  buttonLoading.forEach(function (event) {
+    $(this).click(function(){
+        console.log('Đang Tải Dữ Liệu. Bạn chờ một chút nha !');
     });
   });
-});
