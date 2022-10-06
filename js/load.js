18423ae1472ec0580889d7e6210e139386d4d559
Trung Phạm <trungnhot12@gmail.com>
2022-10-06 19:56:51 +0700

    up

--- js/load.js
+++ js/load.js
@@ -6,18 +6,18 @@ else {
   couponShow.addClass("row");
   loadContent(couponShow, "coupon.html");
 
-  var loadedContent = false;
-  $(window).on("scroll", function () {
-    if (
-      !loadedContent &&
-      $(window).scrollTop() + $(window).height() >= loadContents.offset().top
-    ) {
-      loadedContent = true;
-      if (urlParams["type"] == "shopee")
-        loadContent(loadContents, "shopee.html");
-      else if (urlParams["type"] == "tiki")
-        loadContent(loadContents, "tiki.html");
-      else loadContent(loadContents, "allvoucher.html");
-    }
-  });
+  // var loadedContent = false;
+  // $(window).on("scroll", function () {
+  // if (
+  //   !loadedContent &&
+  //   $(window).scrollTop() + $(window).height() >= loadContents.offset().top
+  // ) {
+  // loadedContent = true;
+  if (urlParams["type"] == "shopee")
+    loadContent(loadContents, "shopee.html");
+  else if (urlParams["type"] == "tiki")
+    loadContent(loadContents, "tiki.html");
+  else loadContent(loadContents, "allvoucher.html");
+  // }
+  // });
 }
