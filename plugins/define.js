47275aabe3e0bb5124e77566c4617ba23e320179
Trung Pham <trungnhot12@gmail.com>
2022-10-06 20:52:19 +0700

    up

--- plugins/define.js
+++ plugins/define.js
@@ -1,4 +1,45 @@
-// define 
+// define
 function buttonLoadingToast() {
-    $.niceToast.info('Đang Tải Dữ Liệu. Bạn chờ một chút nha !');
-  }
+  $.niceToast.info("Đang Tải Dữ Liệu. Bạn chờ một chút nha !");
+}
+function waitForElementToDisplay(
+  selector,
+  callback,
+  checkFrequencyInMs,
+  timeoutInMs
+) {
+  var startTimeInMs = Date.now();
+  (function loopSearch() {
+    if (document.querySelector(selector) != null) {
+      callback();
+      return;
+    } else {
+      setTimeout(function () {
+        if (timeoutInMs && Date.now() - startTimeInMs > timeoutInMs) return;
+        loopSearch();
+      }, checkFrequencyInMs);
+    }
+  })();
+}
+
+const buttonLoading = [
+  ".atEQPOIVFSDFSDG-nav-link",
+  ".atEQPOIVFSDFSDG-page-link",
+  ".atEQPOIVFSDFSDG-btn-search",
+  ".atEQPOIVFSDFSDG-btn-keyword",
+];
+waitForElementToDisplay(
+  ".atEQPOIVFSDFSDG-modal",
+  function () {
+    buttonLoading.forEach(function (el) {
+      var buttons = document.querySelectorAll(el);
+      for (var button of buttons) {
+        button.addEventListener("click", function (event) {
+          buttonLoadingToast();
+        });
+      }
+    });
+  },
+  1000,
+  9000
+);
