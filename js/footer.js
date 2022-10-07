caf0af3b2dcf10576a01a7702efd9ca86cf2dd1e
Trung Pham <trungnhot12@gmail.com>
2022-10-08 00:18:54 +0700

    up

--- js/footer.js
+++ js/footer.js
@@ -11,29 +11,29 @@ $(function () {
   $("#game").click(function () {
     location.href = currenturl + "?type=game";
   });
-});
 
-var loadscript = document.createElement("script");
-loadscript.type = "text/javascript";
+  var loadscript = document.createElement("script");
+  loadscript.type = "text/javascript";
 
-if (urlParams["type"]) {
-  $("#" + urlParams["type"] + "").addClass("btn-active");
-  // loadscript.src = "js/load.js";
-} else {
-  $("#allvoucher").addClass("btn-active");
-  $("#sortCoupon").show();
-  // loadscript.setAttribute("lazy", "");
-  // loadscript.setAttribute("data-src", "js/load.js");
-}
-loadscript.setAttribute("lazy", "");
-loadscript.setAttribute("data-src", "js/load.js");
-$("#loadScripts").append(loadscript);
-
-$("#sortCouponSelect").change(function () {
-  if (!urlParams["type"]) {
-    var sort = document.getElementById("sortCouponSelect").value;
-    window.location.href = currenturl + "?sort=" + sort;
+  if (urlParams["type"]) {
+    $("#" + urlParams["type"] + "").addClass("btn-active");
+    // loadscript.src = "js/load.js";
   } else {
-    $.niceToast.error("Tính năng này chỉ hoạt động ở trang chủ :(");
+    $("#allvoucher").addClass("btn-active");
+    $("#sortCoupon").show();
+    // loadscript.setAttribute("lazy", "");
+    // loadscript.setAttribute("data-src", "js/load.js");
   }
+  loadscript.setAttribute("lazy", "");
+  loadscript.setAttribute("data-src", "js/load.js");
+  $("#loadScripts").append(loadscript);
+
+  $("#sortCouponSelect").change(function () {
+    if (!urlParams["type"]) {
+      var sort = document.getElementById("sortCouponSelect").value;
+      window.location.href = currenturl + "?sort=" + sort;
+    } else {
+      $.niceToast.error("Tính năng này chỉ hoạt động ở trang chủ :(");
+    }
+  });
 });
