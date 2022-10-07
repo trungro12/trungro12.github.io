145a9e2821b00b42b9cd36bf90e059f7da124ab5
Trung Pham <trungnhot12@gmail.com>
2022-10-07 22:17:25 +0700

    up

--- config/define.js
+++ config/define.js
@@ -5,7 +5,11 @@ $.niceToast.setup({
 });
 
 const at_token = "Token BSlThjyssppl-1bbVJDKRiOBxK9rakro";
-const sp_aff_link = "https://shope.ee/an_redir?affiliate_id=17321760029&origin_link=";
+const sp_aff_link =
+  "https://shope.ee/an_redir?affiliate_id=17321760029&origin_link=";
 
 var currenturl = location.href.replace("#", "").split("?")[0];
 let urlParams = getUrlParams(location.search);
+
+var limitCoupon = 12;
+var limitCouponMobile = 4;
