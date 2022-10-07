576a3c1788c956c831f58f0c2157d081d834c100
Trung Pham <trungnhot12@gmail.com>
2022-10-08 00:36:50 +0700

    up

--- js/load.js
+++ js/load.js
@@ -1,13 +1,14 @@
-// coupon
-var couponShow = $("#coupon-show");
-couponShow.addClass("row");
-if (urlParams["type"] == "game" || !urlParams["type"])
-  loadContent(couponShow, "coupon.html");
-else loadContent(couponShow, "couponv1.html");
+$(function () {
+  // coupon
+  var couponShow = $("#coupon-show");
+  couponShow.addClass("row");
+  if (urlParams["type"] == "game" || !urlParams["type"])
+    loadContent(couponShow, "coupon.html");
+  else loadContent(couponShow, "couponv1.html");
 
-var loadContents = $("#loadContents");
-if (urlParams["type"] == "shopee") loadContent(loadContents, "shopee.html");
-else if (urlParams["type"] == "tiki") loadContent(loadContents, "tiki.html");
-else if (urlParams["type"] == "game") loadContent(loadContents, "game.html");
-else loadContent(loadContents, "allvoucher.html");
-
+  var loadContents = $("#loadContents");
+  if (urlParams["type"] == "shopee") loadContent(loadContents, "shopee.html");
+  else if (urlParams["type"] == "tiki") loadContent(loadContents, "tiki.html");
+  else if (urlParams["type"] == "game") loadContent(loadContents, "game.html");
+  else loadContent(loadContents, "allvoucher.html");
+});
