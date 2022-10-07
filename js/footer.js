2a61ac82cb07561ba649a55accbe9e518038fc8a
Trung Pham <trungnhot12@gmail.com>
2022-10-07 23:36:28 +0700

    up

--- js/footer.js
+++ js/footer.js
@@ -37,3 +37,7 @@ $("#sortCouponSelect").change(function () {
     $.niceToast.error("Tính năng này chỉ hoạt động ở trang chủ :(");
   }
 });
+if(urlParams["sort"])
+{
+  document.getElementById("sortCouponSelect").value = parseInt(urlParams["sort"]);
+}
