7d296a9406eb35b2a431bee5cb4d31dc39b3ffa3
Trung Phạm <trungnhot12@gmail.com>
2022-10-08 18:44:20 +0700

    up

--- js/load.js
+++ js/load.js
@@ -15,6 +15,14 @@ couponShow.addClass("row");
 if (urlParams["type"] == "game" || !urlParams["type"])
   {
     loadContent(couponShow, "coupon.html");
-    $("#sortCoupon").show();
+    var shortCouponHTML = '';
+    shortCouponHTML += '<label for="sortCouponSelect" style="color: #fff;margin-bottom: -10px;">Sắp xếp coupon theo</label><br>';
+    shortCouponHTML += '<select name="sortCouponSelect" id="sortCouponSelect">';
+    shortCouponHTML += '<option value="0">&#xf004; Coupon Mới nhất</option>';
+    shortCouponHTML += '<option selected value="4">&#xf0e7; Coupon Đang hot</option>';
+    shortCouponHTML += '<option value="2">&#xf06d; Coupon Dùng nhiều</option>';
+    shortCouponHTML += '<option value="3">&#xf017; Coupon Sắp Hết Hạn</option>';
+    shortCouponHTML += '</select>';        
+    $("#sortCoupon").html(shortCouponHTML);
 }
 else loadContent(couponShow, "couponv1.html");
