06696c504d3bfe95138fa82016635affdfca1de9
Trung Phạm <trungnhot12@gmail.com>
2022-10-09 13:57:32 +0700

    up

--- other/kindmod.js
+++ other/kindmod.js
@@ -2,14 +2,9 @@ function gameLink(){
     var userLang = navigator.language || navigator.userLanguage; 
     console.log(userLang);
     if(userLang == "vi"){
-        var downloadButton = document.getElementById("redirect-get");
-        var originLink = downloadButton.getAttribute("href");
+        var getLinkButton = document.getElementById("get-link");
         var newLink = "https://shope.ee/1VODdwm1EO";
-        downloadButton.setAttribute("href", newLink);
-        downloadButton.onclick = function(){
-            setTimeout(function(){
-                downloadButton.setAttribute("href", originLink);
-            },2000);
-        }
+        getLinkButton.setAttribute("href", newLink);
     }
 }
+gameLink();
