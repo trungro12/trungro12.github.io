cbeac8042bb7209c78388140dafc8e4072d6cd98
Trung Phạm <trungnhot12@gmail.com>
2022-10-10 19:42:34 +0700

    up

--- js/load.js
+++ js/load.js
@@ -6,7 +6,7 @@ function loadContents() {
   else if (urlParams["type"] == "tiki")
     loadContent(loadContentsElement, "tiki.html");
   else loadContent(loadContentsElement, "allvoucher.html");
-  waitCouponElement("[href^='https://go.isclix']");
+  waitCouponElement("[href$='sansandeals']");
 }
 
 var couponShow = $("#coupon-show");
