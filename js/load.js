969459e1d07e5a4d9c2de86fd92952d97810cf8b
Trung Phạm <trungnhot12@gmail.com>
2022-10-11 22:16:11 +0700

    up

--- js/load.js
+++ js/load.js
@@ -1,51 +1,55 @@
-var loadContentsElement = $("#loadContents");
-function loadContents() {
-  $("#more-coupon").hide();
-  if (urlParams["type"] == "shopee")
-    loadContent(loadContentsElement, "shopee.html");
-  else if (urlParams["type"] == "tiki")
-    loadContent(loadContentsElement, "tiki.html");
-  else loadContent(loadContentsElement, "allvoucher.html");
-}
-
-var couponShow = $("#coupon-show");
-couponShow.addClass("row");
-
 $(function () {
+  var loadContentsElement = $("#loadContents");
+  function loadContents() {
+    $("#more-coupon").hide();
+    if (urlParams["type"] == "shopee")
+      loadContent(loadContentsElement, "shopee.html");
+    else if (urlParams["type"] == "tiki")
+      loadContent(loadContentsElement, "tiki.html");
+    else loadContent(loadContentsElement, "allvoucher.html");
+    waitCouponElement("#tabsdeal--content");
+  }
+
+  var couponShow = $("#coupon-show");
+  couponShow.addClass("row");
+
+
   if (urlParams["type"] != "game") couponWidget("#sortCoupon");
+
+
+  if (!urlParams["type"]) {
+    var sortCouponHTML = "";
+    sortCouponHTML +=
+      '<label for="sortCouponSelect" style="color: #fff;margin-bottom: -10px;">Sắp xếp coupon theo</label><br>';
+    sortCouponHTML += '<select name="sortCouponSelect" id="sortCouponSelect">';
+    sortCouponHTML += '<option value="0">&#xf004; Coupon Mới nhất</option>';
+    sortCouponHTML += '<option value="4">&#xf0e7; Coupon Đang hot</option>';
+    sortCouponHTML += '<option value="2">&#xf06d; Coupon Dùng nhiều</option>';
+    sortCouponHTML += '<option value="3">&#xf017; Coupon Sắp Hết Hạn</option>';
+    sortCouponHTML += "</select>";
+
+    var sortType = sortCouponDefault;
+    if (urlParams["sort"] || parseInt(urlParams["sort"]) == 0)
+      sortType = parseInt(urlParams["sort"]);
+    var searchText = 'value="' + sortType + '">';
+    sortCouponHTML = sortCouponHTML.replace(searchText, "selected " + searchText);
+
+    $("#sortCoupon").html(sortCouponHTML).hide();
+
+    loadContent(couponShow, "coupon.html");
+    $("#sortCouponSelect").change(function () {
+      if (!urlParams["type"]) {
+        var sort = document.getElementById("sortCouponSelect").value;
+        window.location.href = currenturl + "?sort=" + sort;
+      } else {
+        document.getElementById("sortCouponSelect").value = sortCouponDefault;
+        $.niceToast.error("Tính năng này chỉ hoạt động ở trang chủ :(");
+      }
+    });
+  } else if (urlParams["type"] == "game") {
+  } else loadContent(couponShow, "couponv1.html");
+
 });
-
-if (!urlParams["type"]) {
-  var sortCouponHTML = "";
-  sortCouponHTML +=
-    '<label for="sortCouponSelect" style="color: #fff;margin-bottom: -10px;">Sắp xếp coupon theo</label><br>';
-  sortCouponHTML += '<select name="sortCouponSelect" id="sortCouponSelect">';
-  sortCouponHTML += '<option value="0">&#xf004; Coupon Mới nhất</option>';
-  sortCouponHTML += '<option value="4">&#xf0e7; Coupon Đang hot</option>';
-  sortCouponHTML += '<option value="2">&#xf06d; Coupon Dùng nhiều</option>';
-  sortCouponHTML += '<option value="3">&#xf017; Coupon Sắp Hết Hạn</option>';
-  sortCouponHTML += "</select>";
-
-  var sortType = sortCouponDefault;
-  if (urlParams["sort"] || parseInt(urlParams["sort"]) == 0)
-    sortType = parseInt(urlParams["sort"]);
-  var searchText = 'value="' + sortType + '">';
-  sortCouponHTML = sortCouponHTML.replace(searchText, "selected " + searchText);
-
-  $("#sortCoupon").html(sortCouponHTML).hide();
-
-  loadContent(couponShow, "coupon.html");
-  $("#sortCouponSelect").change(function () {
-    if (!urlParams["type"]) {
-      var sort = document.getElementById("sortCouponSelect").value;
-      window.location.href = currenturl + "?sort=" + sort;
-    } else {
-      document.getElementById("sortCouponSelect").value = sortCouponDefault;
-      $.niceToast.error("Tính năng này chỉ hoạt động ở trang chủ :(");
-    }
-  });
-} else if (urlParams["type"] == "game") {
-} else loadContent(couponShow, "couponv1.html");
 
 // loadcache when fail call ajax
 function loadCouponCache() {
